6dd0645cb7a21da375323a22ff662ad4fe1f157f
Marcelo Andrade <marcelo.m.andrade@outlook.com>
2022-04-13 21:42:21 -0300

    Ajuste de diretorio

--- MMandrade.Estudos.ImportarArquivo.ConsoleApp/Program.cs
+++ MMandrade.Estudos.ImportarArquivo.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using MMandrade.Estudos.ImportarArquivo.ConsoleApp.Models;
 using MMandrade.Estudos.ImportarArquivo.ConsoleApp.Services;
 
@@ -11,24 +13,26 @@ namespace MMandrade.Estudos.ImportarArquivo.ConsoleApp
             Console.WriteLine("Inicializando a leitura do arquivo CSV");
             
             var _estudanteService = new EstudanteServices();
-            var caminho = @"../MMandrade.Estudos.ImportarArquivo.ConsoleApp/Estudante.csv";
+            //var caminho = @"./MMandrade.Estudos.ImportarArquivo.ConsoleApp/Estudante.csv";
+            var caminhoLeitura = @"../../../Estudante.csv";
+            var caminhoEscrita = @"../../../ArquivoEscrito.csv";
 
             //Faz a chamada da função para leitura do arquivo CSV
-             var resultData = _estudanteService.LerArquivoCSV(caminho);
+             var resultData = _estudanteService.LerArquivoCSV(caminhoLeitura);
 
             //Gravação de um arquivo CSV
             //Criar um objeto estudante:
             Estudante estudante = new Estudante();
-                estudante.Id = 3;
+                estudante.Id = 4;
                 estudante.Nome = "Aluno de teste";
                 estudante.Curso = "Analise e desenvolvimento de sistemas";
                 estudante.ValorMensalidades = 25000;
-                estudante.Celular = "055111234567890";
+                estudante.Celular = "00123456";
                 estudante.Cidade = "Mogi das Cruzes";
                 resultData.Add(estudante);
 
                 //Chamada da função para escrita do CSV
-                _estudanteService.EscreverArquivoCSV(@"../MMandrade.Estudos.ImportarArquivo.ConsoleApp/ArquivoEscrito.csv",resultData);
+                _estudanteService.EscreverArquivoCSV(caminhoEscrita, resultData);
                 
                 Console.WriteLine("Arquivo gravado");
 
